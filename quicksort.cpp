dfe6432e7b77cbe65cfc390d61627be790197d79
Sheery <sk9109182@gmail.com>
2024-05-13 10:17:40 +0500

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,52 +1,43 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int partition(int arr[], int s, int e) {
-	int pivot = arr[s];
-	int cnt = 0;
-	for(int i=s+1; i<=e; i++) {
-		if(arr[i]<=pivot){
-			cnt++;
-		}
-		
-	}
-	int pivotIndex = s+cnt;
-	swap(arr[pivotIndex], arr[s]);
-	int i =s, j =e;
-	while(i<pivotIndex && j>pivotIndex) {
-		while(arr[i] < pivot)
-		{
-			i++;
-		}
-		while(arr[j] > pivot)
-		{
-			j--;
-		}
-		if(i<pivotIndex && j>pivotIndex){
-			swap(arr[i++], arr[j--]);
-		}
-	}
-	return pivotIndex;
+int partition(int arr[],int low,int high)
+{
+  int pivot=arr[high];
+  int i=(low-1);
+  
+  for(int j=low;j<=high;j++)
+  { 
+    if(arr[j]<pivot)
+    {
+      i++;
+      swap(arr[i],arr[j]);
+    }
+  }
+  swap(arr[i+1],arr[high]);
+  return (i+1);
+}
+           
+void quickSort(int arr[],int low,int high)
+{
+  if(low<high)
+  { 
+    int pi=partition(arr,low,high);
+    quickSort(arr,low,pi-1);
+    quickSort(arr,pi+1,high);
+  }
+}
+             
+ 
+int main() {
+  int arr[]={10,7,8,9,1,5};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  quickSort(arr,0,n-1);
+  cout<<"Sorted Array\n";
+  for(int i=0;i<n;i++)
+  {
+    cout<<arr[i]<<" ";
+  }
+  return 0;
 }
 
-void quickSort(int arr[], int s, int e){
-	if (s>=e)
-	return ;
-	
-	int p = partition(arr,s,e);
-	quickSort(arr, s, p-1);
-	quickSort(arr, p+1, e); 
-	quickSort(arr, p+1, e);
-}
-
-int main(){
-	int arr[5]={8,5,1,7,0};
-	int n = 5;
-	quickSort(arr,0,n-1);
-	for(int i=0; i<n; i++)
-	{
-		cout<<arr[i]<< " ";
-		
-	} cout<< endl;
-	return 0;
-}
